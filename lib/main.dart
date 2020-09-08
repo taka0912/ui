67803910dbdfd951ead3450514691970649fee3e
taka <volley.0912@icloud.com>
2020-09-08 23:44:41 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,30 +17,32 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Instagram'),
-        actions: <Widget>[
-          Icon(
-            Icons.message,
-          )
-        ],
-      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            height: 10,
+            height: 120,
+            padding: EdgeInsets.only(top: 50, right: 20, left: 20, bottom: 10),
             child: Row(
               children: <Widget>[
-                Scaffold(
-                  body: Container(
+                Expanded(
+                  child: Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(50),
                         color: Colors.grey[200]
                     ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search, color: Colors.grey,),
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(color: Colors.grey),
+                        hintText: "Search",
+                      ),
                     ),
                   ),
+                ),
                 SizedBox(width: 20,),
+                Icon(Icons.camera_alt, color: Colors.grey[800], size: 30,)
               ],
             ),
           ),
@@ -48,53 +50,69 @@ class _HomePageState extends State<HomePage> {
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: Padding(
-                padding: EdgeInsets.all(5),
+                padding: EdgeInsets.all(20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Text("Stories", style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold, fontSize: 22, letterSpacing: 1.2),),
+                        Text("See Archive"),
+                      ],
+                    ),
+                    SizedBox(height: 20,),
                     Container(
-                      height: 140,
+                      height: 180,
                       child: ListView(
                         scrollDirection: Axis.horizontal,
                         children: <Widget>[
                           makeStory(
+                              storyImage: 'assets/images/hukei3.jpg',
                               userImage: 'assets/images/story/this3.jpeg',
-                              userName: 'Hukushi Sota'
-
+                              userName: 'Takeru Sato'
                           ),
                           makeStory(
-                              userImage: 'assets/images/story/this2.jpeg',
-                              userName: 'Yoshizawa Ryo'
-                          ),
-                          makeStory(
+                              storyImage: 'assets/images/sakura.jpg',
                               userImage: 'assets/images/story/this1.jpeg',
-                              userName: 'Yokohama Ryusei'
+                              userName: 'ひょっこりはん'
                           ),
                           makeStory(
+                              storyImage: 'assets/images/hukei.webp',
+                              userImage: 'assets/images/story/this4.jpeg',
+                              userName: '瀬戸 康史'
+                          ),
+                          makeStory(
+                              storyImage: 'assets/images/hukei4.jpg',
                               userImage: 'assets/images/story/this.jpeg',
-                              userName: 'Nikotyan'
+                              userName: 'Sota Hukushi'
                           ),
                         ],
                       ),
                     ),
-                    SizedBox(height: 0,),
+                    SizedBox(height: 40,),
                     makeFeed(
-                        userName: 'Hukushi Sota',
-                        userImage: 'assets/images/story/this.jpeg',
-                        feedText: 'Look at this cute hamster!',
-                        feedImage: 'assets/images/sakura.jpg'
-                    ),
-                    makeFeed(
-                        userName: 'Yoshizawa Ryo',
+                        userName: 'みやぞん',
                         userImage: 'assets/images/story/this5.jpeg',
-                        feedText: "毎日の努力の積み重ねが大切。本気で努力すれば、結果はあとからついてくる！",
-                        feedImage: ''
+                        feedTime: '1 hr ago',
+                        feedText: '収録終わりー',
+                        feedImage: 'assets/images/hukei2.jpg'
                     ),
                     makeFeed(
-                        userName: 'Yokohama Ryusei',
-                        userImage: 'assets/images/story/this4.jpeg',
-                        feedText: "今日の朝ごはんはパンでした。",
-                        feedImage: 'assets/images/sakura.jpg'
+                        userName: 'Azamat Zhanisov',
+                        userImage: 'assets/images/story/this2.jpeg',
+                        feedTime: '3 mins ago',
+                        feedText: "All the Lorem Ipsum generators on the Internet tend to repeat predefined.All the Lorem Ipsum generators on the Internet tend to repeat predefined.All the Lorem Ipsum generators on the Internet tend to repeat predefined.",
+                        feedImage: 'assets/images/hukei.jpg'
+                    ),
+                    makeFeed(
+                        userName: 'Azamat Zhanisov',
+                        userImage: 'assets/images/story/this3.jpeg',
+                        feedTime: '3 mins ago',
+                        feedText: "All the Lorem Ipsum generators on the Internet tend to repeat predefined.",
+                        feedImage: 'assets/images/hukei5.jpg'
                     ),
                   ],
                 ),
@@ -107,35 +125,52 @@ class _HomePageState extends State<HomePage> {
   }
 
 
-  Widget makeStory({ userImage, userName}) {
+  Widget makeStory({storyImage, userImage, userName}) {
     return AspectRatio(
-      aspectRatio: 1.2 / 2,
+      aspectRatio: 1.6 / 2,
+      child: Container(
+        margin: EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+              image: AssetImage(storyImage),
+              fit: BoxFit.cover
+          ),
+        ),
         child: Container(
           padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  colors: [
+                    Colors.black.withOpacity(.9),
+                    Colors.black.withOpacity(.1),
+                  ]
+              )
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
-                width: 60,
-                height: 60,
+                width: 40,
+                height: 40,
                 decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(color: Colors.white, width: 2),
                     image: DecorationImage(
                         image: AssetImage(userImage),
                         fit: BoxFit.cover
-
                     )
                 ),
               ),
-              Text(userName, style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
-              ),)
+              Text(userName, style: TextStyle(color: Colors.white),)
             ],
           ),
         ),
-      );
+      ),
+    );
   }
 
   Widget makeFeed({userName, userImage, feedTime, feedText, feedImage}) {
@@ -164,8 +199,9 @@ class _HomePageState extends State<HomePage> {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(userName, style: TextStyle(color: Colors.grey[900], fontSize: 12, fontWeight: FontWeight.bold, letterSpacing: 1),),
+                      Text(userName, style: TextStyle(color: Colors.grey[900], fontSize: 18, fontWeight: FontWeight.bold, letterSpacing: 1),),
                       SizedBox(height: 3,),
+                      Text(feedTime, style: TextStyle(fontSize: 15, color: Colors.grey),),
                     ],
                   )
                 ],
@@ -223,15 +259,6 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget makeLike() {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Icon(Icons.favorite_border, size: 25,),
-      ),
-    );
-  }
-
-  Widget make() {
     return Container(
       width: 25,
       height: 25,
